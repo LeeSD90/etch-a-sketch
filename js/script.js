dffbca03b2901b479bb2cbe74df038d85cb10e1a
LeeSD90 <lee.doyle90@gmail.com>
2015-08-16 19:38:53 +0100

    Basic Etch-A-Sketch setup completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-	$('body').append("<p>Hello</p>");
+	createGrid(64);
+	monitor();
 });
+
+function createGrid(gridSize){
+	var squareSize = (512/gridSize);
+	var totalSquares = gridSize * gridSize;
+
+	for(i = 0; i < totalSquares; i++){
+		$('#grid').prepend('<div class="square"></div>');
+	}
+
+	$(".square").width(squareSize);
+	$(".square").height(squareSize);
+
+}
+
+function monitor(){
+	$(".square").hover(function(){
+		$(this).css("background-color", "black");
+	});
+}
