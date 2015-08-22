da3ba1168d5b1766a15209351e9262dd94e17868
LeeSD90 <lee.doyle90@gmail.com>
2015-08-22 11:53:09 +0100

    Added unbinding for old event handlers when toggling drawing mode

--- js/script.js
+++ js/script.js
@@ -1,44 +1,7 @@
 //Need to add something to start the drawing on an initial refresh
 //Figure out how to stop the listeners in each colour function? Couldnt work this out tonight while implementing a gradient effect.
 //Will start over next time instead
-$(document).ready(function() {
-	var color = $("input[name=color_select]:checked").val();
-	var draw = true;
-
-	createGrid($("input[name=grid_size]").val());
-
-
-	$(':button').click(function(){
-		switch($(this).attr('id')){
-			case 'create':
-				createGrid($("input[name=grid_size]").val());
-				break;
-			default:
-				break;
-		}
-	})
-
-	$("#grid").click(function(){
-		draw = !draw;
-	});
-
-	$('input[name=color_select]').change(function(){
-		color = $("input[name=color_select]:checked").val();
-
-		switch(color){
-			case 'black':
-				drawBlack();
-				break;
-			case 'random':
-				drawRandom();
-				break;
-			default:
-				break;
-		}
-	});
-
-});
-
+//fiddler?
 function createGrid(gridSize){
 	$("#grid").empty();
 
@@ -53,8 +16,22 @@ function createGrid(gridSize){
 	$(".square").height(squareSize);
 }
 
+function draw(color){
+	switch(color){
+		case 'black':
+			drawBlack();
+			break;
+		case 'random':
+			drawRandom();
+			break;
+		default:
+			break;
+	}
+}
+
 function drawBlack(){
 	$(".square").hover(function(){
+		console.log("black");
 		$(this).css("background-color", "black");
 	});
 }
@@ -65,3 +42,31 @@ function drawRandom(){
 		$(this).css("background-color", "rgb("+rgbVals+")");
 	});
 }
+
+$(document).ready(function() {
+	var color = $("input[name=color_select]:checked").val();
+	var drawing = true;
+
+	createGrid($("input[name=grid_size]").val());
+
+	$(':button').click(function(){
+		switch($(this).attr('id')){
+			case 'create':
+				createGrid($("input[name=grid_size]").val());
+				break;
+			default:
+				break;
+		}
+	})
+
+	$("#grid").click(function(){
+		drawing = !drawing;
+	});
+
+	$('input[name=color_select]').change(function(){
+		color = $("input[name=color_select]:checked").val();
+		$('.square').off("mouseenter mouseleave");		//Important to unbind old hover event handlers!
+		draw(color);
+	});
+
+});
